d9318c3a23f1865cbfa8747629b5bff55bb71d28
edzrna <edzrna@gmail.com>
2026-03-21 02:45:25 -0700

    Actualizar index.js

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,14 @@
-import { useEffect } from "react";
-
 export default function Home() {
-  useEffect(() => {
-    console.log("WojakMeter mounted ✅");
-  }, []);
-
   return (
     <div style={{
-      minHeight: "100vh",
       background: "#071018",
       color: "#fff",
+      minHeight: "100vh",
       display: "flex",
       alignItems: "center",
-      justifyContent: "center",
-      fontFamily: "Arial"
+      justifyContent: "center"
     }}>
-      <h1>WojakMeter Running 🚀</h1>
+      <h1>WojakMeter LIVE 🚀</h1>
     </div>
   );
 }
