b33653b4024011bab1cb907ee039db16c229f0e4
edzrna <edzrna@gmail.com>
2026-03-22 22:43:57 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,1 +1,17 @@
-y
+import Head from "next/head";
+import "@/styles/globals.css";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@500;600;700&family=Rajdhani:wght@500;600;700&family=Inter:wght@400;500;600&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
+}
