d92d877582ef11534f83dd0261e119a7e357491a
edzrna <edzrna@gmail.com>
2026-04-01 19:52:32 -0600

    Crear signal.js

---
+++ pages/api/signal.js
@@ -0,0 +1,14 @@
+import { computeMarketSignal } from "@/lib/signal-engine";
+
+export default async function handler(req, res) {
+  try {
+    const r = await fetch("https://api.coingecko.com/api/v3/global");
+    const json = await r.json();
+
+    const signal = computeMarketSignal(json.data);
+
+    res.status(200).json(signal);
+  } catch (err) {
+    res.status(500).json({ error: "signal error" });
+  }
+}
