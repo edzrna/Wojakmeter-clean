e119a5efc4a3024547765d9e7f7429178fe2814e
edzrna <edzrna@gmail.com>
2026-05-01 10:10:20 -0700

    Actualizar og.js

--- pages/api/og.js
+++ pages/api/og.js
@@ -5,27 +5,31 @@ export const config = {
 };
 
 export default function handler(req) {
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          width: "1200px",
-          height: "630px",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          background: "#071018",
-          color: "#4dff88",
-          fontSize: "80px",
-          fontWeight: 800,
-        }}
-      >
-        WOJAKMETER OG TEST
-      </div>
-    ),
-    {
-      width: 1200,
-      height: 630,
-    }
-  );
+  try {
+    return new ImageResponse(
+      (
+        <div
+          style={{
+            width: "1200px",
+            height: "630px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            background: "#071018",
+            color: "#4dff88",
+            fontSize: "80px",
+            fontWeight: 800,
+          }}
+        >
+          WOJAKMETER OG TEST
+        </div>
+      ),
+      {
+        width: 1200,
+        height: 630,
+      }
+    );
+  } catch (e) {
+    return new Response("OG ERROR: " + e.message, { status: 500 });
+  }
 }
