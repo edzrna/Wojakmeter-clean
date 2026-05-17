051eb1dd022aca2daeb3805d1c398c962a21218a
edzrna <edzrna@gmail.com>
2026-05-16 20:49:18 -0700

    Actualizar top-coins.js

--- pages/api/top-coins.js
+++ pages/api/top-coins.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
           cgUrl("/coins/markets", {
             vs_currency: "usd",
             order: "market_cap_desc",
-            per_page: 10,
+            per_page: 20,
             page: 1,
             sparkline: "false",
             price_change_percentage: "1h,24h,7d"
