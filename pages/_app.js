d7188d2c62f5a6b4bb48e4be958a4b568cddbeaf
Eduardo Cerna Garcia <edzrna@gmail.com>
2026-08-20 22:00:47 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import "../styles/globals.css";
 import "../styles/wm-organism.css";
+import "../styles/legal.css";
+import "../styles/about.css";
 
 
 export default function App({ Component, pageProps }) {
